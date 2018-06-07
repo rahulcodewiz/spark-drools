9b65c825dacd1389ce9e261fb17a13c89c4e97c8
rahulsquid <rahul.shrm@live.in>
2018-06-06 23:06:41 -0700

    Adding base rules

--- batch/src/main/java/com/ts/blog/batch/Launcher.java
+++ batch/src/main/java/com/ts/blog/batch/Launcher.java
@@ -1,6 +1,6 @@
 package com.ts.blog.batch;
 
-public class Test {
+public class Launcher {
     public static void main(String[] args) {
         CategoryAssignmentApp.main(new String[]{"-i", "/Users/rahul/git/spark-drools/input-data/blog.csv","-o", "/Users/rahul/git/spark-drools/input-data/"});
 
